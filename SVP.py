6d5509ed0167c5cd9c939683674117326b5ec50b
Daniel Huseman <huseman@tamu.edu>
2020-03-25 15:19:14 -0500

    Add files via upload

--- SVP.py
+++ SVP.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
 import math
-import sys
-import matplotlib
-
-from typing import Any
+import matplotlib.pyplot as plt
+from tkinter.filedialog import askopenfilename()
 
 
 # Calculates:   Surface Gravity(m/s)
@@ -12,11 +10,11 @@ from typing import Any
 def calc_surface_gravity(lat):
     sin_rad_lat = math.sin(math.radians(lat))
     gravity = (1 + ((sin_rad_lat ** 2) * .0052788) + ((sin_rad_lat ** 4) * .0000236)) * 9.780318
-    print('surface gravity at', lat, 'deg:', round(gravity, 6), 'm/s^2')
+    # print('surface gravity at', lat, 'deg:', round(gravity, 6), 'm/s^2')
     return gravity
 
 
-# Calculates: Gravity at depth(m/s)
+# Calculates: Gravity at depth(m/s^2)
 # Input: Surface Gravity(m/s), pressure(dbar)
 def calc_gravity_depth(sg, pressure):  # UNESCO Gravity equation
     gravity_depth = .5 * 0.000002184 * pressure + sg
@@ -31,8 +29,13 @@ def calc_depth(pressure, gravity_depth):
     depth3 = ((((pressure * 1.45038) * 0.689475728) ** 3) * .0000000002279)
     depth4 = ((((pressure * 1.45038) * 0.689475728) ** 4) * .00000000000000182)
     depth = (depth1 + depth2 + depth3 + depth4) / gravity_depth
-    # print('at', pressure, 'dbar, depth is equal to', round(depth, 3), 'm,', round(depth*3.28083, 3), 'ft')
     return depth
+
+
+def graph(graph_points):
+    plt.plot(graph_points[0], graph_points[1])
+    plt.ylim(0, len())
+    plt.show()
 
 
 def main():
@@ -47,19 +50,23 @@ def main():
     sonardyne_pro = open('%s.pro' % fn, 'w')
     sonardyne_pro.write(site)
     sonardyne_pro.write('\n')
-    depth_dup_check = 0                                        # depth_dup_check checks for duplicate pressure readings
+    depth_dup_check = 0  # depth_dup_check checks for duplicate pressure readings
     profile_check = 1
+    graph_points = [[], []]
+    duplicate_count = 0
 
     for line in dat_lines[28:]:
         pressure = float(line.split()[3])
         sound_velocity = float(line.split()[2])
         gravity_depth = calc_gravity_depth(surface_gravity, pressure)
         depth = calc_depth(pressure, gravity_depth)
+
         # print('depth', depth, '   DDC', depth_dup_check)
-        print(profile_check, '-', round(depth), '=', profile_check - round(depth), round(depth_dup_check))
+        # print(profile_check, '-', round(depth), '=', profile_check - round(depth), round(depth_dup_check))
 
         if round(depth) == round(depth_dup_check):
-            print('Duplicate detected:', round(depth))
+            # print('Duplicate detected:', round(depth))
+            duplicate_count += 1
             continue
 
         sonardyne_pro.write(str(round(depth)))
@@ -67,16 +74,27 @@ def main():
         sonardyne_pro.write(str(sound_velocity))
         sonardyne_pro.write('\n')
 
+        graph_points[0].append(sound_velocity)
+        graph_points[1].append(round(depth))
+
         depth_dup_check = depth
         profile_check += 1
 
     sonardyne_pro.close()
 
+    print('\nREPORT')
+    print('Total number of samples collected:', len(dat_lines) - 28)
+    print('Number of duplicates:', duplicate_count)
+    print('Latitude:', latitude)
+    print('Surface Gravity:', round(surface_gravity, 4), 'm/s^2')
+    print('Average Sound Velocity:', round(sum(graph_points[0]) / len(graph_points[0]), 3), 'm/s')
+
+    plt.plot(graph_points[0], graph_points[1])
+    plt.ylim(len(dat_lines), 0)
+    plt.title(site + ' Sound Velocity Profile')
+    plt.xlabel('Sound Velocity (m/s)')
+    plt.ylabel('Depth (m)')
+    plt.show()
+
 
 main()
-
-# TODO: loop thru sv data and calculate sv for every meter of depth
-
-# TODO: graph sv through the water column
-
-# TODO: write a .pro file and fill with values
